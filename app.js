65e4830027dfbeeb99cad191d26fe2217e5ff4cb
Talhamalik495 <mohammadtalhamalik495@gmail.com>
2024-05-27 21:51:09 +0500

    bulb on of

---
+++ app.js
@@ -0,0 +1,10 @@
+const lighton = () => {
+    let a = document.getElementById("image")
+    a.src = "images/on-removebg-preview.png"
+
+}
+const lightof = () => {
+    let a = document.getElementById("image")
+    let c = a.src = "images/of-removebg-preview.png"
+    c.src = "images/of-removebg-preview.png"
+}
